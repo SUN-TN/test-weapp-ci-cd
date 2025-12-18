716b340c9ed952b6121aa947afba7ab7e01f4afe
SUN-TN <1035153751@qq.com>
2025-12-18 11:27:28 +0800

    refactor: 移除多余文件，只保留最基础代码

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,49 +1,11 @@
 import { View } from "@tarojs/components";
-import { Button, TextArea } from "@nutui/nutui-react-taro";
-import { observer } from "mobx-react-lite";
-import { useAppContext } from "@/context";
-import { navigateTo } from "@tarojs/taro";
-import { TestApi } from "@/api/test";
 
-import "./index.scss";
-import { useState } from "react";
-function Index() {
-  const { userStore } = useAppContext();
-  const [name, setName] = useState(userStore.name);
-  const onGetArticle = async () => {
-    const res = await TestApi.getArticle({
-      pageSize: 10,
-      pageIndex: 1,
-      searchKey: "测试",
-    });
-    console.log(res);
-  };
-  const changeName = () => {
-    userStore.setName(name);
-  };
+import styles from "./index.module.scss";
+export default function Index() {
+ 
   return (
-    <View className="nutui-react-demo">
-      <View>{userStore.name}</View>
-      <View>
-        <TextArea
-          showCount
-          maxLength={20}
-          placeholder="请输入姓名"
-          onChange={setName}
-        />
-        <Button type="primary" onClick={changeName}>
-          changeName
-        </Button>
-        <Button
-          type="primary"
-          onClick={() => navigateTo({ url: "/pages/page2/index" })}
-        >
-          goPage2
-        </Button>
-        <Button onClick={onGetArticle}>测试接口</Button>
-      </View>
+    <View className={styles.taroApp}>
+      taro app
     </View>
   );
 }
-
-export default observer(Index);
