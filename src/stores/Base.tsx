be5160744c763a5c2ae72578fea0b705b26563b0
SUN-TN <1035153751@qq.com>
2025-12-22 09:39:38 +0800

    refactor(stores): 优化BaseStore类中的mobx装饰器逻辑
    添加对computed属性和setter的支持
    统一字符串使用双引号格式

--- src/stores/Base.tsx
+++ src/stores/Base.tsx
@@ -1,8 +1,8 @@
-import { makeObservable, observable, action } from 'mobx';
-import { makePersistable } from 'mobx-persist-store';
+import { makeObservable, observable, action, computed } from "mobx";
+import { makePersistable } from "mobx-persist-store";
 
 export default abstract class BaseStore {
-  data_updated_at = '';
+  data_updated_at = "";
 
   // 记录默认值
   protected _defaults = {} as { [K in keyof this]: this[K] };
@@ -13,8 +13,8 @@ export default abstract class BaseStore {
      */
     this._defaults = Object.fromEntries(
       Object.keys(this)
-        .filter((i) => i !== '_defaults')
-        .map((key) => [key, this[key as keyof this]]),
+        .filter((i) => i !== "_defaults")
+        .map((key) => [key, this[key as keyof this]])
     ) as {
       [K in keyof this]: this[K];
     };
@@ -23,22 +23,37 @@ export default abstract class BaseStore {
      * 声明属性和方法
      */
     const instanceKeys = Object.getOwnPropertyNames(this);
-    const prototypeKeys = Object.getOwnPropertyNames(Object.getPrototypeOf(this));
+    const prototypeKeys = Object.getOwnPropertyNames(
+      Object.getPrototypeOf(this)
+    );
 
     const keys = [...new Set([...instanceKeys, ...prototypeKeys])];
 
     // 构造装饰器映射对象
-    const annotations: Record<string, typeof observable | typeof action> = {
+    const annotations: Record<
+      string,
+      typeof observable | typeof action | typeof computed
+    > = {
       update: action,
       restore: action,
     };
 
     for (const key of keys) {
-      if (['constructor', '_defaults'].includes(key)) continue;
+      if (["constructor", "_defaults"].includes(key)) continue;
 
       const value = (this as Record<string, unknown>)[String(key)];
 
-      if (typeof value === 'function') {
+      const descriptor =
+        Object.getOwnPropertyDescriptor(this, String(key)) ||
+        Object.getOwnPropertyDescriptor(
+          Object.getPrototypeOf(this),
+          String(key)
+        );
+      if (descriptor && typeof descriptor.get === "function") {
+        annotations[key] = computed;
+      } else if (descriptor && typeof descriptor.set === "function") {
+        annotations[key] = action;
+      } else if (typeof value === "function") {
         annotations[key] = action;
       } else {
         annotations[key] = observable;
@@ -64,7 +79,7 @@ export default abstract class BaseStore {
     if (!this) return;
 
     for (const key of Object.keys(this) as (keyof this)[]) {
-      if (key === '_defaults') continue;
+      if (key === "_defaults") continue;
 
       this[key] = this._defaults[key];
     }
